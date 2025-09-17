d2360eca5d9d52bd5e225c2c1e5ffedbdcd33536
Yozzy Lazzy <77936231+yozzylazzy@users.noreply.github.com>
2025-09-17 23:06:20 +0700

    implement hero section web

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
+import Hero from "./sections/Hero";
+import NavBar from "./sections/NAvBar";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
-      <div>
-        <h1 className="text-3xl text-pink">
-          Welcome to the world of GTA and GSAP!
-        </h1>
-      </div>
+      <NavBar />
+      <Hero />
     </main>
   );
 };
